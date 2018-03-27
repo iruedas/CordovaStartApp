32ee2a6ac02a6c9d91aca2e4b522abd2f9509e0c
unknown <Antechdev@antechdevcrm.antechlab.com>
2018-03-27 14:19:59 -0700

    Adding windows project

---
+++ src/windows/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenApp
+{
+    public sealed class App
+    {
+        public async void Start(string url)
+        {
+            var uri = new Uri(url);
+            var success = await Windows.System.Launcher.LaunchUriAsync(uri);
+        }
+
+    }
+}
